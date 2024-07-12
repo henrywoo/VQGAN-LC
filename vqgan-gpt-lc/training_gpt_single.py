f65205ef721d43612bf1fc368a4f912e0d7f82d1
Henry Wu <wufuheng@gmail.com>
2024-07-11 18:51:40 -0700

    ffhq

--- vqgan-gpt-lc/training_gpt_single.py
+++ vqgan-gpt-lc/training_gpt_single.py
@@ -9,8 +9,8 @@ import torch.nn.functional as F
 import util.lr_sched as lr_sched
 from torchvision import utils as vutils
 from models.models_gpt import VQGANTransformer
-#from models.maskgit import VQGANBidTransformer
-#from util.utils import load_data, plot_images
+# from models.maskgit import VQGANBidTransformer
+# from util.utils import load_data, plot_images
 import util.misc as misc
 import torch.backends.cudnn as cudnn
 from torch.utils.data import Dataset
@@ -21,13 +21,15 @@ from torchvision import utils as vutils
 from PIL import Image
 from torch.utils.tensorboard import SummaryWriter
 from util.misc import NativeScalerWithGradNormCount as NativeScaler
-from hiq.cv_torch import ImageLabelDataSet, get_cv_dataset, DS_PATH_IMAGENET1K, DS_PATH_DOGFOOD_3K
+from hiq.cv_torch import ImageLabelDataSet, get_cv_dataset, DS_PATH_IMAGENET1K, DS_PATH_DOGFOOD_3K, DS_PATH_FFHQ256_70K
 from hiq.memory import total_gpu_memory_mb_nvml
+
 
 class LabelSmoothing(nn.Module):
     """
     NLL loss with label smoothing.
     """
+
     def __init__(self, smoothing=0.0):
         """
         Constructor for the LabelSmoothing module.
@@ -46,13 +48,15 @@ class LabelSmoothing(nn.Module):
         loss = self.confidence * nll_loss + self.smoothing * smooth_loss
         return loss.mean()
 
-class MyDSINE1KForGPT(ImageLabelDataSet):
+
+class MyDSForGPT_INE1K(ImageLabelDataSet):
     def __init__(self, dataset, transform=None, return_type='pair', split='train', image_size=224, convert_rgb=True,
                  img_key=None):
         super().__init__(dataset, transform, return_type, split, image_size, convert_rgb, img_key)
         self.rescaler = albumentations.SmallestMaxSize(max_size=image_size)
         self.cropper = albumentations.RandomCrop(height=image_size, width=image_size)
         self.preprocessor = albumentations.Compose([self.rescaler, self.cropper])
+
     def __getitem__(self, idx):
         item = self.dataset[idx]
         img = item[self.img_key]
@@ -64,6 +68,25 @@ class MyDSINE1KForGPT(ImageLabelDataSet):
         image = image.transpose(2, 0, 1)
         label = item[self.label_key]
         return [0, image, label]
+
+class MyDSForGPT_FFHQ25670K(ImageLabelDataSet):
+    def __init__(self, dataset, transform=None, return_type='pair', split='train', image_size=224, convert_rgb=True,
+                 img_key=None):
+        super().__init__(dataset, transform, return_type, split, image_size, convert_rgb, img_key)
+        self.rescaler = albumentations.SmallestMaxSize(max_size=image_size)
+        self.cropper = albumentations.RandomCrop(height=image_size, width=image_size)
+        self.preprocessor = albumentations.Compose([self.rescaler, self.cropper])
+
+    def __getitem__(self, idx):
+        item = self.dataset[idx]
+        img = item[self.img_key]
+        if not img.mode == "RGB":
+            img = img.convert("RGB")
+        img = np.array(img).astype(np.uint8)
+        image = self.preprocessor(image=img)["image"]
+        image = (image / 127.5 - 1.0).astype(np.float32)
+        image = image.transpose(2, 0, 1)
+        return [-1, image, -1]
 
 class FFHQDataset(Dataset):
     def __init__(self, data_root, image_size, max_words=30, n_class=1000, partition="train", device="cpu"):
@@ -82,16 +105,18 @@ class FFHQDataset(Dataset):
 
         self.partition = partition
 
-        self.clip_mean = torch.from_numpy(np.array([0.48145466, 0.4578275, 0.40821073])).unsqueeze(0).unsqueeze(-1).unsqueeze(-1).float()#.to(args.device).float()
-        self.clip_std = torch.from_numpy(np.array([0.26862954, 0.26130258, 0.27577711])).unsqueeze(0).unsqueeze(-1).unsqueeze(-1).float()#.to(args.device).float()
+        self.clip_mean = torch.from_numpy(np.array([0.48145466, 0.4578275, 0.40821073])).unsqueeze(0).unsqueeze(
+            -1).unsqueeze(-1).float()  # .to(args.device).float()
+        self.clip_std = torch.from_numpy(np.array([0.26862954, 0.26130258, 0.27577711])).unsqueeze(0).unsqueeze(
+            -1).unsqueeze(-1).float()  # .to(args.device).float()
 
         self.image_ids = []
         self.class_labels = []
 
-        #files = os.listdir(data_root + "/" + partition)
-        #for file in files:
+        # files = os.listdir(data_root + "/" + partition)
+        # for file in files:
         #    self.image_ids.append(partition + "/" + file)
-    
+
         with open("ffhq_split/" + partition + ".txt") as f:
             for line in f.readlines():
                 image_id = line.strip('\n')
@@ -117,11 +142,11 @@ class FFHQDataset(Dataset):
 
 def configure_optimizers(model, args):
     decay, no_decay = set(), set()
-    whitelist_weight_modules = (nn.Linear, )
+    whitelist_weight_modules = (nn.Linear,)
     blacklist_weight_modules = (nn.LayerNorm, nn.Embedding)
 
     for mn, m in model.transformer.named_modules():
-        #print(m)
+        # print(m)
         for pn, p in m.named_parameters():
             fpn = f"{mn}.{pn}" if mn else pn
             if pn.endswith("bias"):
@@ -152,7 +177,6 @@ if __name__ == '__main__':
         help="Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus",
     )
     parser.add_argument("--epochs", default=100, type=int)
-    
     parser.add_argument("--num_workers", default=0, type=int)
     parser.add_argument(
         "--pin_mem",
@@ -161,21 +185,17 @@ if __name__ == '__main__':
     )
     parser.add_argument("--no_pin_mem", action="store_false", dest="pin_mem")
     parser.set_defaults(pin_mem=True)
-
     # distributed training parameters
     parser.add_argument("--distributed", default=0, type=int)
     parser.add_argument("--world_size", default=1, type=int, help="number of distributed processes")
     parser.add_argument("--local_rank", default=-1, type=int)
     parser.add_argument("--dist_on_itp", action="store_true")
     parser.add_argument("--dist_url", default="env://", help="url used to set up distributed training")
-
     parser.add_argument("--image_size", default=256, type=int, help="number of distributed processes")
     parser.add_argument('--lr', type=float, default=4.5e-04, help='Commitment loss scalar.')
     parser.add_argument(
         "--min_lr", type=float, default=1e-05, metavar="LR", help="lower lr bound for cyclic schedulers that hit 0"
     )
-
-
     parser.add_argument('--latent-dim', type=int, default=1024, help='Latent dimension n_z.')
     parser.add_argument('--image-size', type=int, default=256, help='Image height and width.)')
     parser.add_argument('--num-codebook-vectors', type=int, default=1024, help='Number of codebook vectors.')
@@ -191,12 +211,11 @@ if __name__ == '__main__':
     parser.add_argument('--disc-start', type=int, default=10000, help='When to start the discriminator.')
     parser.add_argument('--disc-factor', type=float, default=1., help='Weighting factor for the Discriminator.')
     parser.add_argument('--l2-loss-factor', type=float, default=1., help='Weighting factor for reconstruction loss.')
-    parser.add_argument('--perceptual-loss-factor', type=float, default=1., help='Weighting factor for perceptual loss.')
-    
+    parser.add_argument('--perceptual-loss-factor', type=float, default=1.,
+                        help='Weighting factor for perceptual loss.')
     parser.add_argument("--warmup_iterators", type=int, default=5000, metavar="N", help="epochs to warmup LR")
     parser.add_argument("--keeplr_iterators", type=int, default=80000, metavar="N", help="epochs to warmup LR")
     parser.add_argument("--max_iterators", type=int, default=450000, metavar="N", help="epochs to warmup LR")
-    
     parser.add_argument('--pkeep', type=float, default=0.8, help='Percentage for how much latent codes to keep.')
     parser.add_argument('--sos-token', type=int, default=0, help='Start of Sentence token.')
     parser.add_argument("--output_dir", default="./output_dir", help="path where to save, empty for no saving")
@@ -212,9 +231,8 @@ if __name__ == '__main__':
     parser.add_argument("--use_crossatt_dec", type=int, default=0, help="Decoding Loss")
     parser.add_argument("--seed", default=0, type=int)
     parser.add_argument("--stage", default=2, type=int)
-
-    parser.add_argument("--local_embedding_path", default="cluster_codebook_1000cls_100000.pth", type=str, help="path of llama model")
-
+    parser.add_argument("--local_embedding_path", default="cluster_codebook_1000cls_100000.pth", type=str,
+                        help="path of llama model")
     parser.add_argument("--rate_p", default=0.1, type=float)
     parser.add_argument("--class_condition", default=1, type=int)
     parser.add_argument("--dataset", type=str, default="imagenet", help="")
@@ -231,28 +249,37 @@ if __name__ == '__main__':
     np.random.seed(seed)
     cudnn.benchmark = True
     if args.dataset == "imagenet":
-        dl = get_cv_dataset(path=DS_PATH_DOGFOOD_3K, #DS_PATH_IMAGENET1K
+        dl = get_cv_dataset(path=DS_PATH_DOGFOOD_3K,  # DS_PATH_IMAGENET1K
                             image_size=args.image_size,
                             batch_size=args.batch_size,
                             return_loader=False,
-                            datasetclass=MyDSINE1KForGPT)
+                            datasetclass=MyDSForGPT_INE1K)
         dataset_train, dataset_val = dl['train'], dl['test']
     else:
-        dataset_train = FFHQDataset(
-            data_root=args.imagenet_path, image_size=args.image_size, n_class=args.n_class, partition="train", device=device
+        dl = get_cv_dataset(path=DS_PATH_FFHQ256_70K,
+                            image_size=args.image_size,
+                            batch_size=args.batch_size,
+                            return_loader=False,
+                            datasetclass=MyDSForGPT_FFHQ25670K)
+        dataset_train, dataset_val = dl['train'], None
+        '''dataset_train = FFHQDataset(
+            data_root=args.imagenet_path, image_size=args.image_size, n_class=args.n_class, partition="train",
+            device=device
         )
         dataset_val = FFHQDataset(
-            data_root=args.imagenet_path, image_size=args.image_size, n_class=args.n_class, partition="val", device=device
-        )
+            data_root=args.imagenet_path, image_size=args.image_size, n_class=args.n_class, partition="val",
+            device=device
+        )'''
 
     num_tasks = misc.get_world_size()
     global_rank = misc.get_rank()
     sampler_train = torch.utils.data.DistributedSampler(
         dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True, drop_last=True
     )
-    sampler_val = torch.utils.data.DistributedSampler(
-        dataset_val, num_replicas=num_tasks, rank=global_rank, shuffle=True, drop_last=False
-    )
+    if dataset_val:
+        sampler_val = torch.utils.data.DistributedSampler(
+            dataset_val, num_replicas=num_tasks, rank=global_rank, shuffle=True, drop_last=False
+        )
 
     print("Sampler_train = %s" % str(sampler_train))
 
@@ -265,24 +292,25 @@ if __name__ == '__main__':
         drop_last=True,
     )
 
-    data_loader_val = torch.utils.data.DataLoader(
-        dataset_val,
-        sampler=sampler_val,
-        batch_size=args.batch_size,
-        num_workers=args.num_workers,
-        pin_memory=args.pin_mem,
-        drop_last=True,
-    )
+    if dataset_val:
+        data_loader_val = torch.utils.data.DataLoader(
+            dataset_val,
+            sampler=sampler_val,
+            batch_size=args.batch_size,
+            num_workers=args.num_workers,
+            pin_memory=args.pin_mem,
+            drop_last=True,
+        )
 
     model = VQGANTransformer(args).to(device=args.device)
     optimizer = configure_optimizers(model, args)
-    #for param in model.vqgan.parameters():
+    # for param in model.vqgan.parameters():
     #    param.requires_grad = False
     model_engine = model
     for name, param in model.vqgan.named_parameters():
         param.data = param.data.float()
         param.requires_grad = False
-        
+
     if global_rank == 0 and args.output_dir is not None:
         os.makedirs(args.output_dir, exist_ok=True)
         log_writer = SummaryWriter(log_dir=args.output_dir)
@@ -299,11 +327,11 @@ if __name__ == '__main__':
         print(f"Loading Checkpoint {checkpoint} from epoch {start_epoch}")
 
     for epoch in range(start_epoch, args.epochs):
-        print(f"epoch: {epoch}, GPU: {total_gpu_memory_mb_nvml()//1024}GB")
+        print(f"epoch: {epoch}, GPU: {total_gpu_memory_mb_nvml() // 1024}GB")
         if args.distributed:
             data_loader_train.sampler.set_epoch(epoch)
             data_loader_val.sampler.set_epoch(epoch)
-            
+
         metric_logger = misc.MetricLogger(delimiter="  ")
         metric_logger.add_meter("lr", misc.SmoothedValue(window_size=1, fmt="{value:.6f}"))
 
@@ -311,23 +339,19 @@ if __name__ == '__main__':
         print_freq = 10
         metric_logger.add_meter("lr", misc.SmoothedValue(window_size=1, fmt="{value:.6f}"))
 
-        for i, [image_ids, images, label_cls] in enumerate(metric_logger.log_every(data_loader_train, print_freq, header)):
+        for i, [image_ids, images, label_cls] in enumerate(
+                metric_logger.log_every(data_loader_train, print_freq, header)):
             cur_iter = len(data_loader_train) * epoch + i
             if cur_iter >= args.max_iterators:
                 break
             imgs = images.to(device=args.device)
-
-            label_cls = label_cls.unsqueeze(-1).to(device) + args.n_vision_words
-            
-            ###
-            if args.class_condition == 1:
+            if label_cls[0]==-1:
+                label_cls = label_cls.unsqueeze(-1).to(device) + args.n_vision_words
                 loss = model_engine(imgs, label_cls)
             else:
                 loss = model(imgs)
             loss.backward()
             optimizer.step()
-            ###
-
             lr_sched.adjust_learning_rate_gpt(optimizer, cur_iter, args)
             lr = optimizer.param_groups[0]["lr"]
             metric_logger.update(lr=lr)
@@ -340,17 +364,17 @@ if __name__ == '__main__':
             if log_writer is not None and cur_iter % 1000 == 0:
                 epoch_1000x = int(cur_iter)
                 log_writer.add_scalar("Iter/Loss", loss_value_reduce, epoch_1000x)
-        
+
         metric_logger.synchronize_between_processes()
         print("Training Averaged stats:", metric_logger)
-        
+
         if log_writer is not None:
             log_writer.add_scalar("Epoch/Loss", loss_value_reduce, epoch)
 
-        if epoch%5 ==0:
-            #metric_logger = misc.MetricLogger(delimiter="  ")
+        if dataset_val is not None and epoch % 5 == 0:
             header = "Validation Epoch: [{}]".format(epoch)
-            for i, [image_ids, images, label_cls] in enumerate(metric_logger.log_every(data_loader_val, print_freq, header)):
+            for i, [image_ids, images, label_cls] in enumerate(
+                    metric_logger.log_every(data_loader_val, print_freq, header)):
                 cur_iter = len(data_loader_train) * epoch + i
                 imgs = images.to(device=args.device)
                 label_cls = label_cls.unsqueeze(-1).to(device) + args.n_vision_words
@@ -380,8 +404,8 @@ if __name__ == '__main__':
             torch.save(model.state_dict(), os.path.join(args.output_dir, f"transformer_{epoch}.pt"))
         train_stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
         log_stats = {
-                **{f"train_{k}": v for k, v in train_stats.items()},
-                "epoch": epoch#,
+            **{f"train_{k}": v for k, v in train_stats.items()},
+            "epoch": epoch  # ,
         }
         if args.output_dir and misc.is_main_process():
             if log_writer is not None:
@@ -390,8 +414,4 @@ if __name__ == '__main__':
                 f.write(json.dumps(log_stats) + "\n")
 
         if cur_iter >= args.max_iterators:
-                break
-
-    #train_transformer = TrainTransformer(args)
-
-
+            break
